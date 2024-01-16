b1ba112d2992b3c58e1ca40d79bba8e2b3f1c5b6
Marcin Kłopotek <marcin.klopotek@gmail.com>
2024-01-16 13:22:18 +0100

    Add more quotes

--- assets/scripts/quote-loader.js
+++ assets/scripts/quote-loader.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("data/quotes.json")
-        .then(response => response.json())
-        .then(quotes => {
-            const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-            document.getElementById("quote").textContent = randomQuote.quote
-            document.getElementById("author").textContent = randomQuote.author
-        })
-});
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('data/quotes.json', {
+    cache: 'no-cache', headers: {
+      'Cache-Control': 'max-age=604800', // 1 week
+    },
+  })
+    .then(response => response.json())
+    .then(quotes => {
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+      document.getElementById('quote').textContent = randomQuote.quote
+      document.getElementById('author').textContent = randomQuote.author
+    })
+})
